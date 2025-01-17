fb00645a04e2f34d151ec91283a43b8b04096a5a
Jasim Ansari <jasimcec2025@gmail.com>
2025-01-17 19:43:51 +0530

    JS

---
+++ javaScript/java-2/script.js
@@ -0,0 +1,18 @@
+// let a = 5;
+// console.log(a++);
+// console.log(a++);
+
+
+// let a = 5;
+// console.log(++a);
+// console.log(a++);
+// console.log(++a);
+
+
+
+// let a = 5;
+// console.log(--a);
+// console.log(a--);
+// console.log(--a);
+
+
