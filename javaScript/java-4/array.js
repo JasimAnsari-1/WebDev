668459fa30624656de05ec4d1b2cdbb0f65a205a
Jasim Ansari <jasimcec2025@gmail.com>
2025-01-23 02:12:57 +0530

    JS

---
+++ javaScript/java-4/array.js
@@ -0,0 +1,41 @@
+// let stu_name = ["jasim","moanl","raju","Karan"];
+// console.log(stu_name);
+
+// let emp_arr = [];
+// console.log(emp_arr);
+
+
+// let fruits = ['apple','mango','banana'];
+// fruits[0] = 'litchi';
+// console.log(fruits);
+
+
+// let fruits = ['apple','banana','mango'];
+// console.log(fruits);
+// fruits.push('litchi');
+// console.log(fruits);
+
+
+// let fruits = ['apple','banana','mango'];
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+
+
+// let fruits = ['apple','banana','mango'];
+// fruits.unshift('litchi');
+// console.log(fruits);
+
+
+
+// let fruits = ['apple','banana','mango'];
+// fruits.shift();
+// console.log(fruits);
+
+
+// let text = ["A","B","C","D"];
+// text.unshift("a","b","---","z");
+// console.log(text);
+
+
+
