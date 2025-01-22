4c2286abfb3d03aefaec79c2ae572ae9a93c4ca6
Jasim Ansari <jasimcec2025@gmail.com>
2025-01-22 23:33:58 +0530

    JS

--- javaScript/java-2/script.js
+++ javaScript/java-2/script.js
@@ -1,101 +1,101 @@
-let a = 5;
-console.log(a++);
-console.log(a++);
+// let a = 5;
+// console.log(a++);
+// console.log(a++);
 
 
-let a = 5;
-console.log(++a);
-console.log(a++);
-console.log(++a);
-
-
-
-let a = 5;
-console.log(--a);
-console.log(a--);
-console.log(--a);
+// let a = 5;
+// console.log(++a);
+// console.log(a++);
+// console.log(++a);
 
 
 
-Strings
-let fName = "Jasim";
-console.log(fName);
-
-
-let fullName = "Md Jasim Ansari";
-console.log(fullName[10]);
-console.log(fullName.length);
-console.log(fullName.length-1);
-console.log(fullName.length+1);
+// let a = 5;
+// console.log(--a);
+// console.log(a--);
+// console.log(--a);
 
 
 
+// Strings
+// let fName = "Jasim";
+// console.log(fName);
 
 
-let firstName = "Md";
-let middleName = "Jasim";
-let lastName = "Ansari";
-// let fullName = firstName + " " + middleName + " " + lastName;
+// let fullName = "Md Jasim Ansari";
+// console.log(fullName[10]);
+// console.log(fullName.length);
+// console.log(fullName.length-1);
+// console.log(fullName.length+1);
+
+
+
+
+
+// let firstName = "Md";
+// let middleName = "Jasim";
+// let lastName = "Ansari";
+// // let fullName = firstName + " " + middleName + " " + lastName;
+// // console.log(fullName);
+// let fullName = `${firstName} ${middleName} ${lastName}`;
 // console.log(fullName);
-let fullName = `${firstName} ${middleName} ${lastName}`;
-console.log(fullName);
 
 
-let pen = 10;
-let copy = 50;
-console.log(`total price:${pen+copy}`);
+// let pen = 10;
+// let copy = 50;
+// console.log(`total price:${pen+copy}`);
 
 
 
 
 
-conditional statement
-      if
-let x = 20;
-if(x % 2 === 0){
-    console.log("even");
-}
+// conditional statement
+//       if
+// let x = 20;
+// if(x % 2 === 0){
+//     console.log("even");
+// }
 
 
-             if-else
-let x = 21;
-if(x % 2 === 0){
-    console.log("even");
-}
-else {
-    console.log("odd");
-}
+//              if-else
+// let x = 21;
+// if(x % 2 === 0){
+//     console.log("even");
+// }
+// else {
+//     console.log("odd");
+// }
 
 
-          else-if
-let x = 10;
-if(x>0){
-    console.log("Positive");
-}else if(x<0){
-    console.log("Negative");
-}
-else{
-    console.log("Zero");
-}
+//           else-if
+// let x = 10;
+// if(x>0){
+//     console.log("Positive");
+// }else if(x<0){
+//     console.log("Negative");
+// }
+// else{
+//     console.log("Zero");
+// }
 
 
 
 
-         Switch case
-let day = 3;
-switch (day) {
-  case 1:
-    console.log("Monday");
-    break;
-  case 2:
-    console.log("Tuesday");
-    break;
-  case 3:
-    console.log("Wednesday");
-    break;
-  default:
-    console.log("Invalid day");
-}
+//          Switch case
+// let day = 3;
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
 
 
 
--- javaScript/java-3/script.js
+++ javaScript/java-3/script.js
@@ -1,24 +1,38 @@
-alert("This is good or not");
+// alert("This is good or not");
 
-let age = prompt("Enter your age");
-console.log(age);
-
-
-let userName = prompt("Enter the name");
-alert(`Welcome ${userName}`);
-alert("Welcome to the world of game");
+// let age = prompt("Enter your age");
+// console.log(age);
 
 
-string method
-let fullName = "   Md Jasim Ansari    ";
-console.log(fullName.trim());
-console.log(fullName.toUpperCase());
-console.log(fullName.toLowerCase());
+// let userName = prompt("Enter the name");
+// alert(`Welcome ${userName}`);
+// alert("Welcome to the world of game");
+
+
+// // string method
+// let fullName = "   Md Jasim Ansari    ";
+// console.log(fullName.trim());
+// console.log(fullName.toUpperCase());
+// console.log(fullName.toLowerCase());
 
 
 
-let text = "I am learn coding from youtube";
-console.log(text.indexOf("coding"));
-console.log(`coding:${text.indexOf("coding")},youtube:${text.indexOf("youtube")}`);
+// let text = "I am learn coding from youtube";
+// console.log(text.indexOf("coding"));
+// console.log(`coding:${text.indexOf("coding")},youtube:${text.indexOf("youtube")}`);
 
 
+// let text = "javascript";
+// console.log(text.slice(1,4));
+// console.log(text.slice(1));
+// console.log(text.slice(-1));
+// console.log(text.slice(-6));
+// console.log(text.slice(-7,-1));
+
+
+
+// let text = "I am learn coding";
+// console.log(text.replace("coding","development"));
+
+let text = "done ";
+console.log(text.repeat(10));
