fcbe43317ab8ea246087685f3ef7923977403285
Jasim Ansari <jasimcec2025@gmail.com>
2025-01-22 23:06:07 +0530

    JS

--- javaScript/java-3/script.js
+++ javaScript/java-3/script.js
@@ -1,24 +1,24 @@
-// alert("This is good or not");
+alert("This is good or not");
 
-// let age = prompt("Enter your age");
-// console.log(age);
-
-
-// let userName = prompt("Enter the name");
-// alert(`Welcome ${userName}`);
-// alert("Welcome to the world of game");
+let age = prompt("Enter your age");
+console.log(age);
 
 
-// string method
-// let fullName = "   Md Jasim Ansari    ";
-// console.log(fullName.trim());
-// console.log(fullName.toUpperCase());
-// console.log(fullName.toLowerCase());
+let userName = prompt("Enter the name");
+alert(`Welcome ${userName}`);
+alert("Welcome to the world of game");
+
+
+string method
+let fullName = "   Md Jasim Ansari    ";
+console.log(fullName.trim());
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
 
 
 
-// let text = "I am learn coding from youtube";
-// console.log(text.indexOf("coding"));
-// console.log(`coding:${text.indexOf("coding")},youtube:${text.indexOf("youtube")}`);
+let text = "I am learn coding from youtube";
+console.log(text.indexOf("coding"));
+console.log(`coding:${text.indexOf("coding")},youtube:${text.indexOf("youtube")}`);
 
 
