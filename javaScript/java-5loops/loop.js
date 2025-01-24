6d556c3c458a9a692104ab86d18d3aab819b0a3a
Jasim Ansari <jasimcec2025@gmail.com>
2025-01-24 16:57:33 +0530

    JS

---
+++ javaScript/java-5loops/loop.js
@@ -0,0 +1,80 @@
+//for loop
+// for(let i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+//print the table of 7
+// for(let i=7;i<=70;i+=7){
+//     console.log(i);
+// }
+
+//print the table of n
+// let num = prompt('enter the number');
+// for(i=1;i<=10;i++){
+//     console.log(`${num}*${i} = ${num*i}`)
+// }
+
+
+
+
+
+//while loop
+// let i = 0;
+// while(i<=5){
+//     console.log(i);
+//     i++;
+// }
+
+// let num = prompt('enter the number');
+// let i = 1;
+// while(i<=10){
+//     console.log(num*i);
+//     i++;
+// }
+
+
+
+
+
+
+
+// do while loop
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<0)
+
+
+
+// break and continue keyword
+// let i = 1;
+// while(i<=10){
+//     if(i==4){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 1;
+// while(i<=10){
+//     if(i==4){
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// for (let i = 1; i < 7; i++) {
+//     if (i === 3) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
