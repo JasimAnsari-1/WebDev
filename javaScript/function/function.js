3ecfef1838fd18a0c42abaefe50a43926a20306b
Jasim Ansari <jasimcec2025@gmail.com>
2025-01-25 17:20:17 +0530

    JS

---
+++ javaScript/function/function.js
@@ -0,0 +1,34 @@
+// function sum(x,y){
+//     return x+y;
+// }
+// console.log(sum(6,9));
+
+
+// function sub(x,y){
+//     console.log(x-y);
+// }
+// sub(6,5);
+
+
+// function nameAge(name,age){
+//     console.log(`name: ${name} and age: ${age}`);
+// }
+// nameAge("jasim",20);
+
+// function average(a,b,c){
+//     let average = (a+b+c)/3;
+//     console.log(average)
+// }
+// average(2,3,5);
+
+
+// function table(n){
+//     for(let i=1;i<=10;i++){
+//         console.log(n*i);
+//     }
+// }
+// table(5);
+
+
+
+
