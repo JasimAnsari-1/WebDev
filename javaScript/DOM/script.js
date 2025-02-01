86ceba9b6f68ea5bbfbdddeec51209636686fe54
Jasim Ansari <jasimcec2025@gmail.com>
2025-02-01 22:40:14 +0530

    JS

---
+++ javaScript/DOM/script.js
@@ -0,0 +1,31 @@
+//element get by id 
+// let para = document.getElementById("para");
+// para.innerHTML = "Hello";
+
+
+
+// element get by className
+// let boxes = document.getElementsByClassName("box");
+// for(let i=0;i<boxes.length;i++){
+//     boxes[i].style.backgroundColor = "yellow"
+// }
+
+
+
+// element get by TagName
+// let paragraph = document.getElementsByTagName("p");
+// for(let i=0;i<paragraph.length;i++){
+//     paragraph[i].innerHTML = "Hello";
+// }
+
+
+
+//Queryselector
+// let paragraph = document.querySelector("p");
+// paragraph.innerHTML = "Hello";
+
+
+// let paragraph = document.querySelectorAll("h4");
+// for(let i=0;i<paragraph.length;i++){
+//     paragraph[i].innerHTML = "Hello";
+// }
