25a242a650802463c2038922d37d68d411affeac
Jasim Ansari <jasimcec2025@gmail.com>
2025-02-05 22:37:35 +0530

    JS

--- javaScript/java-4/array.js
+++ javaScript/java-4/array.js
@@ -16,76 +16,76 @@
 // console.log(fruits);
 
 
-let fruits = ['apple','banana','mango'];
-console.log(fruits);
-fruits.pop();
-console.log(fruits);
+// let fruits = ['apple','banana','mango'];
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
 
 
-let fruits = ['apple','banana','mango'];
-fruits.unshift('litchi');
-console.log(fruits);
-
-
-
-let fruits = ['apple','banana','mango'];
-fruits.shift();
-console.log(fruits);
-
-
-let text = ["A","B","C","D"];
-text.unshift("a","b","---","z");
-console.log(text);
+// let fruits = ['apple','banana','mango'];
+// fruits.unshift('litchi');
+// console.log(fruits);
 
 
 
-let fruits = ['apple','banana','mango'];
-console.log(fruits.indexOf('apple'));
-console.log(fruits.indexOf('mango'));
-console.log(`apple: ${fruits.indexOf('apple')},banana: ${fruits.indexOf('banana')}`);
+// let fruits = ['apple','banana','mango'];
+// fruits.shift();
+// console.log(fruits);
 
 
-let arr = [2,1,4,3,6,9,0,1,7,1];
-console.log(`${arr.indexOf(1,2)}`);
-
-
-let fruits = ['banana','oranges','apples','mango'];
-console.log(fruits.includes('apples'));
-console.log(fruits.includes('orange'));
+// let text = ["A","B","C","D"];
+// text.unshift("a","b","---","z");
+// console.log(text);
 
 
 
-
-let color1 = ['red','blue','green','yellow'];
-let color2 = ['white','black','brown'];
-console.log(color1.concat(color2));
-console.log(color1.reverse())
-console.log(color2.concat(color1));
-console.log(color1.concat(color2).reverse());
+// let fruits = ['apple','banana','mango'];
+// console.log(fruits.indexOf('apple'));
+// console.log(fruits.indexOf('mango'));
+// console.log(`apple: ${fruits.indexOf('apple')},banana: ${fruits.indexOf('banana')}`);
 
 
+// let arr = [2,1,4,3,6,9,0,1,7,1];
+// console.log(`${arr.indexOf(1,2)}`);
 
-let arr = [23,56,87,32,75,13];
-console.log(arr.slice(2));
-console.log(arr.slice(2,4));
+
+// let fruits = ['banana','oranges','apples','mango'];
+// console.log(fruits.includes('apples'));
+// console.log(fruits.includes('orange'));
 
 
 
 
-let array = [1,2,3,4,5];
-console.log(array.splice(1,2));          //[3,4]
-console.log(array.splice(1,0,6,7));   //[]
-console.log(array.splice(1,2,5,6,7));    //2,3
+// let color1 = ['red','blue','green','yellow'];
+// let color2 = ['white','black','brown'];
+// console.log(color1.concat(color2));
+// console.log(color1.reverse())
+// console.log(color2.concat(color1));
+// console.log(color1.concat(color2).reverse());
+
+
+
+// let arr = [23,56,87,32,75,13];
+// console.log(arr.slice(2));
+// console.log(arr.slice(2,4));
+
+
+
+
+// let array = [1,2,3,4,5];
+// console.log(array.splice(1,2));          //[3,4]
+// console.log(array.splice(1,0,6,7));   //[]
+// console.log(array.splice(1,2,5,6,7));    //2,3
 
 
 
 
 
-let arr = ['Mango','Apple','Banana','Cherry'];
-console.log(arr.sort());
+// let arr = ['Mango','Apple','Banana','Cherry'];
+// console.log(arr.sort());
 
-let arr = [1,10,25,4,2,6]
-console.log(arr.sort((a,b)=>a-b));
+// let arr = [1,10,25,4,2,6]
+// console.log(arr.sort((a,b)=>a-b));
 
 
 
