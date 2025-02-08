ee4d7f688eccc04265fdf45d341547713761f810
Jasim Ansari <jasimcec2025@gmail.com>
2025-02-08 18:02:00 +0530

    JS

--- javaScript/EventInJs/event.js
+++ javaScript/EventInJs/event.js
@@ -4,4 +4,10 @@ btn1.onclick = ()=>{
     let a = 25;
     a++;
     console.log(a)
-}
+};
+
+let box = document.querySelector("#box1");
+box.onmouseover = ()=>{
+    box.style.backgroundColor = "red"
+};
+
