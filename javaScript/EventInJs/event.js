2d431467e8c8e6c31a16615c545ff3cb1e8ee5a4
Jasim Ansari <jasimcec2025@gmail.com>
2025-02-08 18:37:15 +0530

    JS

--- javaScript/EventInJs/event.js
+++ javaScript/EventInJs/event.js
@@ -1,13 +1,36 @@
-let btn1 = document.querySelector("#btn1");
-btn1.onclick = ()=>{
-    console.log("button was click");
-    let a = 25;
-    a++;
-    console.log(a)
-};
+// 
+// let btn1 = document.querySelector("#btn1");
+// btn1.onclick = ()=>{
+//     console.log("button was click");
+//     let a = 25;
+//     a++;
+//     console.log(a)
+// };
 
-let box = document.querySelector("#box1");
-box.onmouseover = ()=>{
-    box.style.backgroundColor = "red"
-};
+// let box = document.querySelector("#box1");
+// box.onmouseover = ()=>{
+//     box.style.backgroundColor = "red"
+// };
+
+
+// let btn1 = document.querySelector("#btn1");
+// btn1.onclick = (evt)=>{
+//     console.log(evt);
+//     console.log(evt.type);
+//     console.log(evt.target);
+//     console.log(evt.clientX,evt.clientY);
+// };
+
+
+
+
+// let box = document.querySelector("#box1");
+// box.addEventListener("mouseover" , ()=>{
+//     box.style.backgroundColor = "red"
+// });
+// box.addEventListener("mouseout" , ()=>{
+//     box.style.backgroundColor = "dodgerblue"
+// });
+
+
 
