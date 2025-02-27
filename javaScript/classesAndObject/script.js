6597faf874ac78098e29d34a2a7e3e166f1b4f10
Jasim Ansari <jasimcec2025@gmail.com>
2025-02-28 04:14:45 +0530

    JS

---
+++ javaScript/classesAndObject/script.js
@@ -0,0 +1,53 @@
+// const student = {
+//     fullName : "Md Jasim Ansari",
+//     marks: 76.6,
+//     printMarks: function () {
+//         console.log("marks = ",this.marks);
+//     },
+// };
+
+
+// const employee = {
+//     calcTax() { 
+//         console.log("tax rate is 10%");
+//     },
+// };
+// const karanArjun = {
+//     salary: 50000,
+// };
+// karanArjun.__proto__ = employee;
+
+
+// class ToyotaCar {
+//     start() {
+//         console.log("start")
+//     }
+//     stop() {
+//         console.log("stop")
+//     }
+// }
+
+// let fortuner = new ToyotaCar();
+
+
+
+
+// class ToyotaCar {
+//     constructor(brand,mileage) {
+//         this.brand = brand;
+//         this.mileage = mileage;
+//     }
+//     start() {
+//         console.log("start")
+//     }
+//     stop() {
+//         console.log("stop")
+//     }
+// }
+    
+// let fortuner = new ToyotaCar();   
+// let swift = new ToyotaCar("Honda",13)
+    
+    
+
+
